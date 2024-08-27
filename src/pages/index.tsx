3960fda166a2cbbe16002ff929155d9da6ad404c
phucyopaz <phucpv@yopaz.vn>
2024-08-27 17:31:41 +0700

    feature: amplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,21 +5,14 @@ import { useTranslation, Trans } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useStore } from "../context/StoreContext";
 import { useRouter } from 'next/router'
-
+import Container from '@mui/material/Container';
 const Homepage = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter()
   const { t, i18n } = useTranslation("common"); // Use the 'common' namespace
   const changeTo = router.locale === 'en' ? 'jp' : 'en'
   return (
     <>
-      <Link href="/" locale={changeTo}>
-        <button>{t("change-locale", { changeTo })}</button>
-      </Link>
-      <Link href="/second-page" locale={changeTo}>
-        <button>{t("change-locale", { changeTo })}</button>
-      </Link>
-      <h1>{t("language")}</h1>
-      <p>{t("title")}</p>
+      <Container maxWidth="sm"></Container>
     </>
   );
 };
